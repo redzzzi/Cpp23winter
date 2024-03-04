010857402980b5781bab1f8e8b3343742d10e622
redzzzi <myqueen1346@naver.com>
2024-03-04 11:51:04 +0900

    standard c input: integer, string

--- tak/practice/week1/stdcin.cpp
+++ tak/practice/week1/stdcin.cpp
@@ -3,11 +3,19 @@
 using namespace std;
 
 int main() {
-  int InputNum;
-  cout << "HelloWorld!" << endl;
-  cout << "Input an Integer: " ;
-  cin >> InputNum;
+  int inputNumber;
+  string inputName;
 
-  cout << "The Number: " << InputNum << endl;
+  cout << "HelloWorld!" << endl;
+
+  cout << "Input an Integer: ";
+  cin >> inputNumber;
+
+  cout << "Input a Name: ";
+  cin >> inputName; //공백 기준으로 앞에 있는 것 출력
+
+  cout << "The Number: " << inputNumber << endl;
+  cout << "The Name: " << inputName << endl;
+
   return 0;
 }
