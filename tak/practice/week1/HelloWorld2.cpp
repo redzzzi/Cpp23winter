a6bb50c97c320edca3d049107ef3e47cc22465ab
redzzzi <myqueen1346@naver.com>
2024-03-04 11:21:18 +0900

    using namespace

---
+++ tak/practice/week1/HelloWorld2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  cout << "Helloworld!" << "\n" << "PNUCSE ManSE" << endl;
+  return 0;
+}
+
+int foo() {
+  cout << "Fooo" << endl;
+  return 1;
+}
