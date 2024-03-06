506435da678c765df76ebbe3d6747dd6991de4b5
redzzzi <myqueen1346@naver.com>
2024-03-06 10:37:16 +0900

    stream

---
+++ tak/practice/week1/Stream.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int i = 10;
+
+  cout << "i: " << i << endl;
+  cout << "i in octal: " << oct << i << endl;
+  cout << "i in hexa: " << hex << i << endl;
+
+  return 0;
+}
