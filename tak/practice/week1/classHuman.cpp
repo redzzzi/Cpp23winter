4277bd43387ec91b0c064052938d43e5588b5564
redzzzi <myqueen1346@naver.com>
2024-03-06 11:31:20 +0900

    classHuman name age

--- tak/practice/week1/classHuman.cpp
+++ tak/practice/week1/classHuman.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 class Human {
-  
+public:
+  string Name = "Tom cruise";
+  int Age = 23;
 };
 
 int main() {
-  cout << "Hello world!" << endl;
+  Human Tom;
+  cout << Tom.Name << endl;
+
   return 0;
 }
