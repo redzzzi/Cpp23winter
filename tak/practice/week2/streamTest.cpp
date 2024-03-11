40c0fd1507ad1483277d590b2508b5e286a6224a
redzzzi <myqueen1346@naver.com>
2024-03-11 11:38:03 +0900

    ofstream

--- tak/practice/week2/streamTest.cpp
+++ tak/practice/week2/streamTest.cpp
@@ -1,24 +1,38 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 
 using namespace std;
 
 int main() {
+  // [1]
   // char Char1 = '\0', Char2 = '\0', Char3 = '\0';
   // cin >> Char1 >> Char2 >> Char3;
   // cout << Char1 << Char2 << Char3;
 
+  // [2]
   // char CStyleStr[10] = {0};
   // cin.get(CStyleStr, 9);
   // cout << CStyleStr << endl;
 
+  // [3]
   // string Name;
   // cin >> Name;
   // cout << "Hi " << Name << endl;
 
-  string Name;
-  getline(cin, Name);
-  cout << "Hi " << Name << endl;
+  // [4]
+  // string Name;
+  // getline(cin, Name);
+  // cout << "Hi " << Name << endl;
+
+  // [5]
+  ofstream myFile;
+  myFile.open("HelloFile.txt", ios_base::out);
+  if (myFile.is_open()) {
+    cout << "File Openend Successfully" << endl;
+    myFile << "My First test file" << endl;
+    myFile << "Hello File";
+  }
 
   return 0;
 }
