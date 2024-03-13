075b092da8158c210b25338b38b700beb51f061f
redzzzi <myqueen1346@naver.com>
2024-03-13 10:47:03 +0900

    return type: pointer, return address

--- tak/practice/week2/Pointer2.cpp
+++ tak/practice/week2/Pointer2.cpp
@@ -1,10 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
 int main() {
   int Age = 30;
   int DogAge = 0;
 
   int* pInteger = &Age;
+  cout << pInteger << endl; // Age의 주솟값을 프린트
 
   pInteger = &DogAge;
+  cout << pInteger << endl; // DogAge의 주솟값을 프린트
 
   return 0;
 }
