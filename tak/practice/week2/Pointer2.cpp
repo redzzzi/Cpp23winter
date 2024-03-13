23a1d6d6f0dd9e2d3809439e1eebc5aabc8f0708
redzzzi <myqueen1346@naver.com>
2024-03-13 11:09:27 +0900

    importance of starting address

--- tak/practice/week2/Pointer2.cpp
+++ tak/practice/week2/Pointer2.cpp
@@ -4,13 +4,17 @@ using namespace std;
 
 int main() {
   int Age = 30;
-  int DogAge = 0;
-
+  int DogAge = 0; // Automatically allocated in the memory by the C++ Compiler
+  // Pointer type variable must access to some variable alive in the memory
   int* pInteger = &Age;
   cout << pInteger << endl; // Age의 주솟값을 프린트
 
   pInteger = &DogAge;
   cout << pInteger << endl; // DogAge의 주솟값을 프린트
 
+  char* pChar;
+  *pChar = 'A';
+  cout << *pChar << endl;
+  // 첫 시작 주소를 알면, 전체 사이즈를 알 수 있음
   return 0;
 }
