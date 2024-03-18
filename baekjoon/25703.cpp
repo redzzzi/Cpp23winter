7c6f9c25d4cfdada0767134c6e825e1a2a01ed78
redzzzi <myqueen1346@naver.com>
2024-03-18 11:21:58 +0900

    baekjoon
    baekjoon

---
+++ baekjoon/25703.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+int main() {
+  int inputNumber = 0;
+  std::cin >> inputNumber;
+  std::cout << "int a;" << std::endl << "int *ptr = &a;" << std::endl;
+  for (int i=2; i<=inputNumber; i++) {
+    std::cout << "int " << std::string(i, '*') << "ptr" << i << " = &ptr" << i-1 << ";" << std::endl;
+  }
+  return 0;
+}
