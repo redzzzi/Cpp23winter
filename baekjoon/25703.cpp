9fd7f07ff6886b68f1a61c7aad53c0a022edf7cf
redzzzi <myqueen1346@naver.com>
2024-03-18 11:45:50 +0900

    baekjoon

--- baekjoon/25703.cpp
+++ baekjoon/25703.cpp
@@ -1,12 +1,21 @@
 #include <iostream>
-#include <string>
 
 int main() {
   int inputNumber = 0;
   std::cin >> inputNumber;
   std::cout << "int a;" << std::endl << "int *ptr = &a;" << std::endl;
+
   for (int i=2; i<=inputNumber; i++) {
-    std::cout << "int " << std::string(i, '*') << "ptr" << i << " = &ptr" << i-1 << ";" << std::endl;
+    std::cout << "int ";
+    for (int j=0; j<i; j++) {
+      std::cout << "*";
+    } 
+    std::cout << "ptr" << i << " = &ptr";
+    if (i == 2) {
+      std::cout << ";" << std::endl;
+    } else {
+      std::cout << i-1 << ";" << std::endl;
+    }
   }
   return 0;
 }
