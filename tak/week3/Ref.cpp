901696e6b7324988a8ecc72ce07b9664df14b7d2
redzzzi <myqueen1346@naver.com>
2024-03-20 11:03:13 +0900

    & reassign

--- tak/week3/Ref.cpp
+++ tak/week3/Ref.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 int main() {
+  /*
   int Original = 30;
   const int& ConstRef = Original; // const is read-only
   // ConstRef = 40; // so it doesn't make sense
@@ -10,5 +11,31 @@ int main() {
   const int& Ref2 = ConstRef; // NO ERROR : Type Match
 
   cout << "Hello world!" << endl;
+  */
+
+  int intOne = 4;
+  int &rSomeRef = intOne;
+
+  cout << intOne << endl;
+  cout << rSomeRef << endl;
+
+  cout << &intOne << endl;
+  cout << &rSomeRef << endl;
+
+  int intTwo = 8;
+  rSomeRef = intTwo;
+
+  cout << "After" << endl;
+
+  cout << intOne << endl;
+  cout << intTwo << endl;
+  cout << rSomeRef << endl;
+
+  cout << &intOne << endl;
+  cout << &intTwo << endl;
+  cout << &rSomeRef << endl;
+
+  // Above works, but DO NOT REASSIGN. It's very confusing.
+
   return 0;
 }
