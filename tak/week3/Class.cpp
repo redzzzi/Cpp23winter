ac8a1ce26d848b5b5baa5dcefe08823a55ad868b
redzzzi <myqueen1346@naver.com>
2024-03-20 11:32:47 +0900

    INSTANCE located in the STACK

---
+++ tak/week3/Class.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Human {
+public:
+  int Age;
+  string Name;
+  string DateOfBirth;
+public:
+  void Talk(string TextToTalk) {
+    cout << TextToTalk << endl;
+  }
+  void Introduce() {
+    cout << "Age: " << Age << endl;
+    cout << "Name: " << Name << endl;
+    cout << "DateOfBirth: " << DateOfBirth << endl;
+  }
+};
+
+int main() {
+  Human Julie;
+
+  Julie.Age = 21;
+  Julie.Name = "Julie Anna";
+  Julie.Introduce();
+
+  cout << "hello world!" << endl;
+  return 0;
+}
