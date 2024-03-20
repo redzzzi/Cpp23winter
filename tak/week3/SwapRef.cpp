4e860ca5bf1b571cd6d04f82bd40568581245a7f
redzzzi <myqueen1346@naver.com>
2024-03-20 11:13:35 +0900

    making swap with references

---
+++ tak/week3/SwapRef.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+void swap (int& rx, int& ry) {
+  int temp;
+
+  temp = rx;
+  rx = ry;
+  ry = temp;
+}
+
+int main() {
+  int x = 10, y = 20;
+  swap(x, y);
+  std::cout << x << " " << y << std::endl;
+
+  return 0;
+}
