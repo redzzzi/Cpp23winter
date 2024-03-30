f7fe60e60fe9995955b298e9c7612f5fe9c1165e
redzzzi <myqueen1346@naver.com>
2024-03-31 01:07:30 +0900

    update

--- tak/week4/BinCreate.cpp
+++ tak/week4/BinCreate.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <iomanip>
 #include <string>
+#include <vector>
 
 using namespace std;
 
@@ -59,6 +60,31 @@ int main()
 
   int MyNumbers[] = {2011, 2052, -525};
   int Solar[2][3] = {{0, 1, 2}, {3, 4, 5}};
+
+  // iterator is very similar to index
+  vector<int> DynArr(3);
+  DynArr[0] = 365;
+  DynArr[1] = -421;
+  DynArr[2] = 789;
+
+  int AnotherNum = 200;
+  DynArr.push_back(AnotherNum);
+
+  cout << "Size: " << DynArr.size() << endl;
+
+  char SayHello[] = {'H', 'e', 'l', 'l', 'o', ' ', 'W', '0'};
+  cout << SayHello << endl;
+  cout << sizeof(SayHello) << endl;
+
+  SayHello[5] = '0';
+  cout << SayHello << endl;
+  cout << sizeof(SayHello) << endl;
+
+  string MyString = "Hello 0W";
+  cout << MyString << endl;
+  string SecString = " Ha Ha";
+  string ConCat = MyString + SecString;
+  cout << ConCat << endl; 
 
   return 0;
 }
