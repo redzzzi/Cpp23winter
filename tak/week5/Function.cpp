9bc13959f301ac972aaffb15aa411880d6b9cf90
redzzzi <myqueen1346@naver.com>
2024-04-01 11:15:26 +0900

    lambda

---
+++ tak/week5/Function.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+double Area(double R);
+double Area(double R, double H);
+/*
+double Area(double R, int H);
+double Area(int R, double H);
+double Area(int R, int H);
+*/
+
+// define an inline function
+inline long DoubleNum(int InputNum) { // inline doesn't use STACK.
+  return InputNum * 2;
+}
+
+double Area(double R) {
+  return R*R*3.14;
+}
+
+double Area(double R, double H) {
+  return R*R*H*3.14;
+}
+
+int main() {
+  cout << "Hello world!" << endl;
+  cout << Area(2.0) << endl;
+  cout << Area(2, 1) << endl;
+  cout << DoubleNum(4) << endl;
+
+  int n = [](int x, int y) { return x + y; }(5, 4); // Lambda function. nameless.
+  cout << n << endl;
+ 
+  return 0;
+}
