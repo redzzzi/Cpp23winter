f94638ebd103739a0c8a8bd364141d953e57cd1c
redzzzi <myqueen1346@naver.com>
2024-04-03 10:54:53 +0900

    class

--- tak/week5/Class.cpp
+++ tak/week5/Class.cpp
@@ -4,14 +4,39 @@
 using namespace std;
 
 class Human {
-private:
+// private:
+//   int Age;
+//   string Name; // No Constructor.
+public:
   int Age;
   string Name; // No Constructor.
+  /*
+  Human() {
+  }
+  // Overloaded contructor
+  Human(int HumanAge) {
+    Age = HumanAge;
+  }
+  */
+  Human(string HumanName, int HumanAge = 24) {
+    Name = HumanName;
+    Age = HumanAge;
+  }
 };
 
 int main() {
   cout << "Hello world!" << endl;
-  Human Eve(21); // Compiler Automatically Human().
+  // Human Eve(21); // Compiler Automatically Human().
+  // Human Tom;
+  // Human Ive("Ive", 21);
+  Human Twix("Twix");
+  cout << Twix.Name << endl;
+  cout << Twix.Age << endl;
+  Human Jenny("Jenny", 25);
+  cout << Jenny.Name << endl;
+  cout << Jenny.Age << endl;
+  // cout << Ive.Name << endl;
+  // cout << Ive.Age << endl;
   // cout << Eve.Age;
 
   return 0;
