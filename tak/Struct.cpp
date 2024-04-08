11d043ad38182067e9ac2aa618b575d70b2fc450
redzzzi <myqueen1346@naver.com>
2024-04-08 11:36:41 +0900

    struct

---
+++ tak/Struct.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+struct Aggregate1 {
+  int num;
+  double pi;
+};
+
+struct Aggregate2 {
+  int num;
+  char hello[6];
+  int impYears[5];
+};
+
+int main() {
+  Aggregate1 a1 {2017, 3.14};
+  Aggregate2 a2 {42, {'h', 'e'}, {1998, 2003}};
+  cout << "Hello world!" << endl;
+
+  return 0;
+}
