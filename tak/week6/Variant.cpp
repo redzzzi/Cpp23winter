b2b1e2ee94be9509caa2f73789aec64d494c00f7
redzzzi <myqueen1346@naver.com>
2024-04-08 11:28:12 +0900

    variant

---
+++ tak/week6/Variant.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <variant>
+
+using namespace std;
+
+int main() {
+  variant<int, double> varSafe;
+  varSafe = 2.0;
+
+  cout << "Hello world!" << endl;
+
+  try {
+    int pi = get<int>(varSafe);
+  } catch(const bad_variant_access& e) {
+    cout << e.what() << endl;
+    cout << "Try yo access int..." << endl;
+  }
+  return 0;
+}
