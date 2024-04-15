86324c5ea798c3b9e5d76fca06c450be2f1cc2ac
redzzzi <myqueen1346@naver.com>
2024-04-15 12:11:01 +0900

    DestroyInstance

--- tak/week7/Prohib.cpp
+++ tak/week7/Prohib.cpp
@@ -4,13 +4,18 @@ using namespace std;
 
 class MonsterDB {
 private:
-  ~MonsterDB(); // stack에서 instance 사용 막고 싶으면 private 이용하기
+  ~MonsterDB() {}; // stack에서 instance 사용 막고 싶으면 private 이용하기
+public:
+  void DestroyInstance(MonsterDB* pInstance) { // 시작 주소를 넘겨줘야해서, 포인터 타입으로 지정!!
+    delete pInstance;
+  }
 };
 
 int main() {
   cout << "Hello World!" << endl;
   // MonsterDB myDatabase; // stack, local variable
-  MonsterDB* myDatabase = new MonsterDB();
+  MonsterDB* myDatabase = new MonsterDB(); // No automatic delete myDatabase!!
+  myDatabase->DestroyInstance(myDatabase);
 
   return 0;
 }
