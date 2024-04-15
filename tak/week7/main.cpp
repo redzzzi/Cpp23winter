6886123d069917bda83cb31023d7ef4c23c6c643
redzzzi <myqueen1346@naver.com>
2024-04-15 11:28:20 +0900

    template<>

--- tak/week7/main.cpp
+++ tak/week7/main.cpp
@@ -2,11 +2,25 @@
 
 using namespace std;
 
-constexpr double GetPI() { return 22.0 / 7; } // 1
-constexpr myaddition(int i, int j) {          // 2
+class Foo {                                       // 4
+public:
+  int _i;
+  constexpr explicit Foo(int i): _i{i} {}
+  constexpr int GetValue() const {
+    return _i;
+  }
+};
+
+template <typename T, int N>                      // 5
+constexpr int length(const T(&)[N]) {
+  return N;
+}
+
+constexpr double GetPI() { return 22.0 / 7; }     // 1
+constexpr int myaddition(int i, int j) {          // 2
   return i + j;
 }
-const myconstaddition(int i, int j) {         // 3
+const int myconstaddition(int i, int j) {         // 3
   return i + j;
 }
 
@@ -38,7 +52,15 @@ int main() {
   const int n = myconstaddition(k, k);
 
   constexpr int x = myaddition(10, 10);
-  constexpr int z = myconstaddition(10, 10);
+  // constexpr int x = myconstaddition(10, 10); // 에러 남
+
+  // foo is const!!!
+  constexpr Foo foo(5);
+  const int nums[] {1, 2, 3, 4};
+  const int nums2[length(nums)*2] {1, 2, 3, 4, 5, 6, 7, 8};
+  cout << GetPI() << endl;
+
+  cout << foo.GetValue() << endl;
 
   return 0;
 }
