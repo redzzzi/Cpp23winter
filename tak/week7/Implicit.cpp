2a0472cc4b863bbf0504a2340d838232a993f311
redzzzi <myqueen1346@naver.com>
2024-04-15 11:13:00 +0900

    explicit

--- tak/week7/Implicit.cpp
+++ tak/week7/Implicit.cpp
@@ -6,16 +6,20 @@ class A {
 public:
   int age;
 
-  A() {}
+  A() { cout << "Default A is created." << endl; }
   A(int myAge) { // implicit conversion
     age = myAge;
-    cout << "A is created." << endl;
+    cout << age << " A is created." << endl;
   }
+  // explicit A(int myAge) {
+  //   age = myAge;
+  //   cout << age << " A is created." << endl;
+  // }
 };
 
 int main() {
   A myA = 11;
   A myA2;
 
-  myA2 = 11;
+  myA2 = 12;
 }
