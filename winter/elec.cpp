e37ec7db092c0212e84de53eb799a6af9aee89c2
redzzzi <myqueen1346@naver.com>
2024-04-15 10:40:24 +0900

    윰니의 과제를 하려다 못한..

---
+++ winter/elec.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  double capacity, efficiency, battery_level, distance;
+  cin >> capacity >> efficiency >> battery_level;
+  const double km_per_kW = capacity / 100 * efficiency;
+  double now = 0;
+  while (now < distance) {
+
+  }
+}
