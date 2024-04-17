dd75f799e25d03da4f893aaaca2386196e7494ca
redzzzi <myqueen1346@naver.com>
2024-04-17 11:44:57 +0900

    isFreshWaterFish constructor

---
+++ tak/week7/ClassHierarch2.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+using namespace std;
+
+class Fish {
+protected: // accessible only to derived classes
+  bool FreshWaterFish;
+  
+public:
+  // Constructor
+  Fish(bool isFreshwater) : FreshWaterFish(isFreshwater) {}
+
+  void swim() {
+    if (FreshWaterFish)
+      cout << "Swim in the lake" << endl;
+    else
+      cout << "Swim in the sea" << endl;
+  }
+};
+
+class Tuna : public Fish {
+public: // public을 해줘야 Tuna constructor 사용 가능
+  // Tuna() {
+  //   FreshWaterFish = false;
+  // }
+  Tuna(): Fish(false) {}
+};
+
+class Carp : public Fish {
+public:
+  // Carp() {
+  //   FreshWaterFish = true;
+  // }
+  Carp(): Fish(false) {}
+};
+
+int main() {
+  cout << "Hello World!" << endl;
+  Carp myLunch;
+  Tuna myDinner;
+
+  myLunch.swim();
+  myDinner.swim();
+  // myLunch.FreshWaterFish = false; // protected member is not accessible from outside the class hierarchy
+
+  return 0;
+}
