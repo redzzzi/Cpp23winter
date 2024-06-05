3152cd72df54c15493dfdbab4f6d785dd2dd2bfc
redzzzi <myqueen1346@naver.com>
2024-06-05 20:50:12 +0900

    02 modified

--- tak/perpractice_final/16_OperatorTypesOverloading/code/02_ImplementConversion_2.cpp
+++ tak/perpractice_final/16_OperatorTypesOverloading/code/02_ImplementConversion_2.cpp
@@ -1,23 +1,23 @@
 #include <iostream>
-#include <sstream>
-#include <string>
+#include <sstream> // ostringstream을 사용하기 위한 헤더 파일
+#include <string>  // c_str 메소드를 사용하기 위한 헤더 파일
 using namespace std;
 
 class Date {
 private:
-  int Day;
+  int Day;             // 날짜를 저장하는 정수형 변수들
   int Month;
   int Year;
 
-  string DateInString;
+  string DateInString; // 날짜를 문자열로 저장하는 변수
 public:
   Date (int InputDay, int InputMonth, int InputYear): Day(InputDay), Month(InputMonth), Year(InputYear) {};
 
-  operator const char*() {
-    ostringstream formattedDate;
+  operator const char*() { // Date 객체를 'const char*' 타입으로 변환하는 연산자
+    ostringstream formattedDate; // 날짜를 문자열로 포맷
     formattedDate << Day << " / " << Month << " / " << Year;
 
-    DateInString = formattedDate.str();
+    DateInString = formattedDate.str(); // 포맷한 결과를 DateInString에 저장하고 그 문자열의 c 스타일 문자열 포인터를 리턴
     return DateInString.c_str();
   }
   Date& operator ++ () {
