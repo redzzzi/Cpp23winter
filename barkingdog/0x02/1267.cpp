16545ed17e8871d6dc1e92fcbcaa1ad21da0c0a3
redzzzi <myqueen1346@naver.com>
2024-07-15 16:37:55 +0900

    백준 : 핸드폰 요금 1. 함수 사용 자제 및 단축식 생각해보기

--- barkingdog/0x02/1267.cpp
+++ barkingdog/0x02/1267.cpp
@@ -1,2 +1,27 @@
 // 핸드폰 요금 - 브론즈3
+#include <bits/stdc++.h>
+using namespace std;
 
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int N;
+  cin >> N;
+  vector<int> min(N, 0);
+  int Y = 0, M = 0;
+  for (int i=0; i<N; i++) cin >> min[i];
+  
+  for (int i: min) {
+    Y +=  (i / 30 + 1) * 10;
+  }
+  for (int i: min) {
+    M +=  (i / 60 + 1) * 15;
+  }
+
+  if (Y < M) cout << "Y " << Y;
+  else if (Y > M) cout << "M " << M;
+  else cout << "Y M " << Y;
+
+  return 0;
+}
