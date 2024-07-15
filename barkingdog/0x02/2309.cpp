c9188e05eb17213532de470382864c741a8dae8b
redzzzi <myqueen1346@naver.com>
2024-07-15 15:57:38 +0900

    백준 : 일곱 난쟁이 1. numeric -> accumulate() 2. found 플래그를 이용한 루프 control 3. for (int i : vec) cout << i;

--- barkingdog/0x02/2309.cpp
+++ barkingdog/0x02/2309.cpp
@@ -1,5 +1,29 @@
 // 일곱 난쟁이 - 브론즈1
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
 
+  vector<int> vec(9);
+  for (int i=0; i<9; i++) {
+    cin >> vec[i];
+  }
+  sort(vec.begin(), vec.end());
+  int diff = accumulate(vec.begin(), vec.end(), 0) - 100;
+  bool found = false;
+
+  for (int i=0; i<9 && !found; i++) {
+    for (int j=0; j<9; j++) {
+      if (vec[i] != vec[j] && vec[i] + vec[j] == diff) {
+        vec.erase(vec.begin() + j);
+        vec.erase(vec.begin() + i);
+        found = true;
+        break;
+      }
+    }
+  }
+  for (int i : vec) cout << i << '\n';
+  return 0;
+}
